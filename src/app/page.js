4aa44f414e303d0b68c85b1fbdcd9d23e80c5dad
kathryngfrancis <kathryngfrancis@gmail.com>
2025-06-18 03:04:30 +1000

    added validation

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,49 @@
+"use client";
 import React from "react";
 
+/* 
+
+
+*/
+
+const CORRECT_CODE = "Fancy Like";
+
 function Home() {
+  const [code, setCode] = React.useState("");
+  const [response, setResponse] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const isCorrect = code === CORRECT_CODE;
+
+    setResponse(
+      isCorrect
+        ? "https://hypernotepad.com/n/3d47c4a246347785"
+        : "Try again later"
+    );
+  }
+
   return (
     <main>
       <h1>Hello There Next!</h1>
-      <p>Trying this again</p>
+
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <input
+            id="auth-code"
+            type="password"
+            required={true}
+            maxLength={60}
+            value={code}
+            onChange={(event) => {
+              setCode(event.target.value);
+            }}
+          />
+          <button>Validate</button>
+        </div>
+      </form>
+      <p>{response}</p>
     </main>
   );
 }
