f24f44326dc7938bb456d43a6362c1fcd432e46a
kathryngfrancis <kathryngfrancis@gmail.com>
2025-06-20 11:07:01 +1000

    made it a p

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import HitCounter from "./HitCounter";
 function Home() {
   return (
     <main>
-      <h1>I'm thinking. Come back later.</h1>
+      <p>I'm thinking. Come back later.</p>
 
       <HitCounter></HitCounter>
     </main>
