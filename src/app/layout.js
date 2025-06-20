0450056bb6173eb15ac0f5e3d80e1c13a64ee768
kathryngfrancis <kathryngfrancis@gmail.com>
2025-06-20 11:04:09 +1000

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,7 @@ import "./styles.css";
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-
-        <form name="makeGuess" method="POST" data-netlify="true" hidden>
-          <input type="text" name="guess" />
-        </form>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
