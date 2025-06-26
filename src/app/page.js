6eabbc1d1f389478c04ce67d515c54869d0f75bc
kathryngfrancis <kathryngfrancis@gmail.com>
2025-06-26 13:07:40 +1000

    new link

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,19 @@ import HitCounter from "./HitCounter";
 import GuessTaker from "./guessTaker";
 
 /* 
-
+https://hypernotepad.com/n/3d47c4a246347785
+The password is the thing you say right before a debut. I think it's
+        meant as a clarification for the audience so they won't be confused.
 
 */
 
 function Home() {
   return (
     <main>
-      <a href="https://hypernotepad.com/n/3d47c4a246347785">
-        https://hypernotepad.com/n/3d47c4a246347785
+      <a href="https://hypernotepad.com/n/596b5c3afd13942d">
+        https://hypernotepad.com/n/596b5c3afd13942d
       </a>
-      <p>
-        The password is the thing you say right before a debut. I think it's
-        meant as a clarification for the audience so they won't be confused.
-      </p>
+      <p>The password is AbleHeart. (no space)</p>
       <HitCounter></HitCounter>
     </main>
   );
